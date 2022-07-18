e29b0c4e159ba54ef1482b8b15f0357821ffad9f
Kyla N. Marjes <kylanmarjes@iskolarngbayan.pup.edu.ph>
2022-07-18 12:54:11 +0800

    add infos part 2

--- pdf resume creator/read json.cs
+++ pdf resume creator/read json.cs
@@ -78,6 +78,7 @@ namespace pdf_resume_creator
                 JuniorHighSchool = "OLONGAPO CITY NATIONAL HIGH SCHOOL",
                 SeniorHighSchool = "GORDON COLLEGE",
                 College = "POLYTECHNIC UNIVERSITY OF THE PHILIPPINES (STA. MESA)",
+                ExperienceAccomplishment = "Salutatorian (2nd honor), Campus Journalist, Active Girl Scout, Consistent MTAP Contestant, Australian Mathematics Qualifier, Science Contestant(District), Campus Journalist (News Writing), CAT Officer(2nd / Lt.Alpha Company 1st Platoon Leader), President's Lister (1st sem)",
                 Competencies1 = "Proficient in 3 programming",
                 Competencies2 = "Fluent in Filipino, English, and Japanese",
                 Competencies3 = "Excellent communication skills",
@@ -108,6 +109,7 @@ namespace pdf_resume_creator
             public string JuniorHighSchool { get; set; }
             public string SeniorHighSchool { get; set; }
             public string College { get; set; }
+            public string ExperienceAccomplishment { get; set; }
             public string Competencies1 { get; set; }
             public string Competencies2 { get; set; }
             public string Competencies3 { get; set; }
