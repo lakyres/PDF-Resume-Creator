9d88addce6f6e70aa9a214e4b463fc158413d1c3
Kyla N. Marjes <kylanmarjes@iskolarngbayan.pup.edu.ph>
2022-07-18 15:23:30 +0800

    write & read json file working and can create pdf with personaldetails (from the json file)

--- pdf resume creator/Form1.Designer.cs
+++ pdf resume creator/Form1.Designer.cs
@@ -30,9 +30,10 @@
         {
             this.lbl_generate = new System.Windows.Forms.Label();
             this.btn_go = new System.Windows.Forms.Button();
-            this.btn_write = new System.Windows.Forms.Button();
+            this.btn_read = new System.Windows.Forms.Button();
             this.btn_jsontest = new System.Windows.Forms.Button();
             this.txt_displayread = new System.Windows.Forms.TextBox();
+            this.btn_write = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lbl_generate
@@ -40,7 +41,7 @@
             this.lbl_generate.AutoSize = true;
             this.lbl_generate.Font = new System.Drawing.Font("Stencil", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_generate.ForeColor = System.Drawing.Color.Transparent;
-            this.lbl_generate.Location = new System.Drawing.Point(230, 41);
+            this.lbl_generate.Location = new System.Drawing.Point(167, 41);
             this.lbl_generate.Name = "lbl_generate";
             this.lbl_generate.Size = new System.Drawing.Size(414, 29);
             this.lbl_generate.TabIndex = 0;
@@ -52,7 +53,7 @@
             this.btn_go.BackColor = System.Drawing.Color.PowderBlue;
             this.btn_go.Font = new System.Drawing.Font("Sylfaen", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_go.ForeColor = System.Drawing.Color.Teal;
-            this.btn_go.Location = new System.Drawing.Point(70, 32);
+            this.btn_go.Location = new System.Drawing.Point(49, 32);
             this.btn_go.Name = "btn_go";
             this.btn_go.Size = new System.Drawing.Size(112, 43);
             this.btn_go.TabIndex = 1;
@@ -60,17 +61,18 @@
             this.btn_go.UseVisualStyleBackColor = false;
             this.btn_go.Click += new System.EventHandler(this.btn_go_Click);
             // 
-            // btn_write
+            // btn_read
             // 
-            this.btn_write.BackColor = System.Drawing.Color.PowderBlue;
-            this.btn_write.Font = new System.Drawing.Font("Sylfaen", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_write.ForeColor = System.Drawing.Color.Teal;
-            this.btn_write.Location = new System.Drawing.Point(59, 136);
-            this.btn_write.Name = "btn_write";
-            this.btn_write.Size = new System.Drawing.Size(132, 43);
-            this.btn_write.TabIndex = 2;
-            this.btn_write.Text = "WRITE";
-            this.btn_write.UseVisualStyleBackColor = false;
+            this.btn_read.BackColor = System.Drawing.Color.PowderBlue;
+            this.btn_read.Font = new System.Drawing.Font("Sylfaen", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_read.ForeColor = System.Drawing.Color.Teal;
+            this.btn_read.Location = new System.Drawing.Point(79, 361);
+            this.btn_read.Name = "btn_read";
+            this.btn_read.Size = new System.Drawing.Size(150, 43);
+            this.btn_read.TabIndex = 2;
+            this.btn_read.Text = "DISPLAY";
+            this.btn_read.UseVisualStyleBackColor = false;
+            this.btn_read.Click += new System.EventHandler(this.btn_write_Click);
             // 
             // btn_jsontest
             // 
@@ -94,15 +96,29 @@
             this.txt_displayread.Size = new System.Drawing.Size(280, 286);
             this.txt_displayread.TabIndex = 4;
             // 
+            // btn_write
+            // 
+            this.btn_write.BackColor = System.Drawing.Color.PowderBlue;
+            this.btn_write.Font = new System.Drawing.Font("Sylfaen", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_write.ForeColor = System.Drawing.Color.Teal;
+            this.btn_write.Location = new System.Drawing.Point(79, 118);
+            this.btn_write.Name = "btn_write";
+            this.btn_write.Size = new System.Drawing.Size(150, 43);
+            this.btn_write.TabIndex = 5;
+            this.btn_write.Text = "WRITE";
+            this.btn_write.UseVisualStyleBackColor = false;
+            this.btn_write.Click += new System.EventHandler(this.btn_write_Click_1);
+            // 
             // PDFcreator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.CadetBlue;
             this.ClientSize = new System.Drawing.Size(692, 525);
+            this.Controls.Add(this.btn_write);
             this.Controls.Add(this.txt_displayread);
             this.Controls.Add(this.btn_jsontest);
-            this.Controls.Add(this.btn_write);
+            this.Controls.Add(this.btn_read);
             this.Controls.Add(this.btn_go);
             this.Controls.Add(this.lbl_generate);
             this.Name = "PDFcreator";
@@ -117,9 +133,10 @@
 
         private System.Windows.Forms.Label lbl_generate;
         private System.Windows.Forms.Button btn_go;
-        private System.Windows.Forms.Button btn_write;
+        private System.Windows.Forms.Button btn_read;
         private System.Windows.Forms.Button btn_jsontest;
         private System.Windows.Forms.TextBox txt_displayread;
+        private System.Windows.Forms.Button btn_write;
     }
 }
 
--- pdf resume creator/Form1.cs
+++ pdf resume creator/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using iTextSharp.text;
 using iTextSharp.text.pdf;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -15,7 +16,7 @@ namespace pdf_resume_creator
 {
     public partial class PDFcreator : Form
     {
-        
+        private readonly string jsonfile = @"C:\Users\angel\source\repos\pdf resume creator\json file\resume.json";
         public PDFcreator()
         {
             InitializeComponent();
@@ -35,8 +36,11 @@ namespace pdf_resume_creator
 
         private void btn_go_Click(object sender, EventArgs e)
         {
+           
+
+
             Document pdf = new Document(PageSize.A4, 20f, 20f, 30f, 30f);
-            PdfWriter pdfwrite = PdfWriter.GetInstance(pdf, new FileStream("C:\\Users\\angel\\source\\repos\\pdf resume creator\\generated pdf\\Resume.pdf", FileMode.Create));
+            PdfWriter pdfwrite = PdfWriter.GetInstance(pdf, new FileStream("C:\\Users\\angel\\source\\repos\\pdf resume creator\\generated pdf\\MARJES_KYLA.pdf", FileMode.Create));
             pdf.Open();
 
             iTextSharp.text.Font titlefont = FontFactory.GetFont(iTextSharp.text.Font.FontFamily.HELVETICA.ToString(), 30,
@@ -54,7 +58,7 @@ namespace pdf_resume_creator
             pdf.Add(mypicturetext);
 
             iTextSharp.text.Font txtfont = FontFactory.GetFont(iTextSharp.text.Font.FontFamily.HELVETICA.ToString(), 12,
-                iTextSharp.text.Font.BOLD, iTextSharp.text.BaseColor.BLACK);
+                iTextSharp.text.Font.NORMAL, iTextSharp.text.BaseColor.BLACK);
 
             Paragraph infos = new Paragraph(txt_displayread.Text, txtfont);
 
@@ -64,6 +68,104 @@ namespace pdf_resume_creator
 
             
             pdf.Close();
+        }
+
+        private void btn_write_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string jsonFile;
+                using (var read = new StreamReader(jsonfile))
+                {
+                    jsonFile = read.ReadToEnd();
+                }
+
+                txt_displayread.Text = jsonFile;
+
+                var resumeFromJson = JsonConvert.DeserializeObject<Resume>(jsonFile);
+            }
+
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+
+        private Resume GetResume()
+        {
+            var resume = new Resume
+            {
+
+                FirstName = "Kyla",
+                MiddleName = "Naranjo",
+                LastName = "Marjes",
+                BirthDate = "October 11, 2002",
+                BirthPlace = "Quezon City",
+                Sex = "Female",
+                CivilStatus = "Single",
+                Citizenship = "Filipino",
+                Religion = "Roman Catholic",
+                Introduction = "My extensive knowledge of computer usage and internet navigation can contribute to your company's innovative and supportive work environment. ",
+                Elementary = "Kalalake Elementary School",
+                JuniorHighSchool = "Olongapo City National High School",
+                SeniorHighSchool = "Gordon College",
+                College = "Polytechnic University of the Phillipines (Sta. Mesa)",
+                Accomplishment = "Salutatorian (2nd honor), Campus Journalist, Active Girl Scout, Consistent MTAP Contestant, Australian Mathematics Qualifier, Science Contestant (District), Campus Journalist (News Writing), CAT Officer (2nd / Lt. Alpha Company 1st Platoon Leader), President's Lister (1st sem)",
+                Competencies = "Proficient in 3 programming; Fluent in Filipino, English, and Japanese; Excellent communication skills ",
+                ContactNumber = "09285797337",
+                Email = "kylamarjes11@gmail.com",
+                Address = "#83-B Irving St., New Kalalake, Olongapo City, Zambales",
+
+            };
+
+            return resume;
+        }
+
+        private class Resume
+        {
+
+            public string FirstName { get; set; }
+            public string MiddleName { get; set; }
+            public string LastName { get; set; }
+            public string BirthDate { get; set; }
+            public string BirthPlace { get; set; }
+            public string Sex { get; set; }
+            public string CivilStatus { get; set; }
+            public string Citizenship { get; set; }
+            public string Religion { get; set; }
+            public string Introduction { get; set; }
+            public string Elementary { get; set; }
+            public string JuniorHighSchool { get; set; }
+            public string SeniorHighSchool { get; set; }
+            public string College { get; set; }
+            public string Accomplishment { get; set; }
+            public string Competencies { get; set; }
+            public string ContactNumber { get; set; }
+            public string Email { get; set; }
+            public string Address { get; set; }
+
+
+
+
+        }
+
+        private void btn_write_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                var resume = GetResume();
+
+                var jsonToWrite = JsonConvert.SerializeObject(resume, Formatting.Indented);
+
+                using (var write = new StreamWriter(jsonfile))
+                {
+                    write.Write(jsonToWrite);
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
         }
     }
 }
